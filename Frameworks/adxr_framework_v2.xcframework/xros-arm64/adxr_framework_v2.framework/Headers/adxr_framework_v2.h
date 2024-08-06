1bd9dd05e5a5ca8219a7bed3512d8ceb6dad2dfb
John Cain <johncain1018@gmail.com>
2024-08-06 00:59:29 -0400

    new framework test

---
+++ Frameworks/adxr_framework_v2.xcframework/xros-arm64/adxr_framework_v2.framework/Headers/adxr_framework_v2.h
@@ -0,0 +1,18 @@
+//
+//  adxr_framework_v2.h
+//  adxr_framework_v2
+//
+//  Created by John Cain on 8/6/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for adxr_framework_v2.
+FOUNDATION_EXPORT double adxr_framework_v2VersionNumber;
+
+//! Project version string for adxr_framework_v2.
+FOUNDATION_EXPORT const unsigned char adxr_framework_v2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <adxr_framework_v2/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "ADXRPackage",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "ADXRPackage",
+            targets: ["adxr_framework_v2"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "adxr_framework_v2",
+            path: "Frameworks/adxr_framework_v2.xcframework")
+    ]
+)
