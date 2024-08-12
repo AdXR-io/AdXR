5e89da82e447f3736ee365629aca3b147d8fcc5f
John Cain <johncain1018@gmail.com>
2024-08-12 14:49:24 -0400

    forgot name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "ADXRPackage",
+            name: "AdXR",
             targets: ["adxr"])
     ],
     targets: [
