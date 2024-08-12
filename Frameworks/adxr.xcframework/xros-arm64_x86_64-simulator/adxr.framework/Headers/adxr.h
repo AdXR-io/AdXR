fd5338f8ad5c1d57ae900d8cb0b35f9689814716
John Cain <johncain1018@gmail.com>
2024-08-12 14:45:58 -0400

    fix for binary naming

---
+++ Frameworks/adxr.xcframework/xros-arm64_x86_64-simulator/adxr.framework/Headers/adxr.h
@@ -0,0 +1,18 @@
+//
+//  adxr.h
+//  adxr
+//
+//  Created by John Cain on 8/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for adxr.
+FOUNDATION_EXPORT double adxrVersionNumber;
+
+//! Project version string for adxr.
+FOUNDATION_EXPORT const unsigned char adxrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <adxr/PublicHeader.h>
+
+
